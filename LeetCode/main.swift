1626bc836acbdea8104d49038c3f4ef1d753138f
wupeng4321 <wupeng_4321@163.com>
2018-05-09 01:20:22 +0800

    03最长子串

--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -86,7 +86,42 @@ public class ListNode {
 //}
 
 
+/*3. 无重复字符的最长子串
 
+*给定一个字符串，找出不含有重复字符的最长子串的长度。
+
+*示例：
+
+*给定 "abcabcbb" ，没有重复字符的最长子串是 "abc" ，那么长度就是3。
+
+*给定 "bbbbb" ，最长的子串就是 "b" ，长度是1。
+
+*给定 "pwwkew" ，最长子串是 "wke" ，长度是3。请注意答案必须是一个子串，"pwke" 是 子序列  而不是子串。
+*/
+func lengthOfLongestSubstring(_ s: String) -> Int {
+    if s.count == 0 || s.count == 1 {return s.count}
+    var a = String(s.first!)
+    var count = 0
+    var ss = s
+    for _ in ss {
+        ss.removeFirst()
+        for cc in ss {
+            if a.contains(cc) {
+//                if a.count > count {count = a.count}
+                break
+            } else {
+                a = a + "\(cc)"
+            }
+            if a.count > count {count = a.count}
+        }
+        if count >= ss.count {break}
+        a = String(ss.first!)
+    }
+    return count
+}
+
+let a = lengthOfLongestSubstring("abcabcbb")
+print(a)
 
 
 
